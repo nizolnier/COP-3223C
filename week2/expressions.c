b6d690613e02a69dc5b9c0bcae49de3c805d7b4e
Nicole <nzolnier@gmail.com>
2021-05-30 17:53:13 -0300

    refactor ex 3

--- week2/expressions.c
+++ week2/expressions.c
@@ -11,10 +11,19 @@ int main(void) {
     int expr1, expr2;
 
     expr1 = a + b * c / 2 - (a * b % 4);
-
     b = expr1 - b;
 
     printf("Expr1 = %d\n", expr1);
-
     printf("b = %d\n", b);
+
+    //Test increment and parenthesis ops
+    expr1 = expr1 + 1;
+    expr2 = (5 * (expr1 + b % a) - 91) * (b - a - c + expr1 / 6);
+    printf("Expr1 = %d\n", expr1);
+    printf("Expr2 = %d\n", expr2);
+
+    //clarify the use of % on negative values
+    printf("-17 mod 4 = %d\n", (-17 % 4));
+    printf("17 mod 4 = %d\n", (17 % -4));
+    printf("-17 mod -4 = %d\n", (-17 % -4));
 }

fbbfa38958cc7dfebc6f713b0337cb8c79534179
Nicole <nzolnier@gmail.com>
2021-06-28 22:49:15 -0300

    ex 1

---
+++ assignment3/program1.c
@@ -0,0 +1,35 @@
+/* Nicole Zolnier - 6/16/21
+Filling a 10 ints array and pritting the numbers
+*/
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+// declaring the size of the array as a const as it will be used many times and it doesnt change
+const int SIZE = 10;
+
+int main (void) {
+    // declaring index and the array of numbers
+    int i, numbers[SIZE];
+    // seeding the random numbers
+    srand(time(0));
+
+    // looping the array to fill it with random numbers
+    for (i = 0; i < SIZE; i++) {
+        numbers[i] = rand() % 100;
+    }
+
+    // nice warning for the user to understand what are these numbers printing out of nowhere
+    printf("This array contains: ");
+
+    // looping to print the numbers :)
+    for (i = 0; i < SIZE; i++) {
+        printf("%d ", numbers[i]);
+    }
+
+    // break the line at the end and that's a wrap!
+    printf("\n");
+
+    return 0;
+
+}
